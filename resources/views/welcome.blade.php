831c0a9684f78cd52d1f98d9a8c14ff70a946e8b
KOTANI\uvmpd <kofma91@gmail.com>
2024-12-05 15:52:37 +0900

    Vue導入

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,11 @@
         @endif
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body>
+        <div id="app"></div>
+        @vite('resources/js/app.js')
+    </body>
+    <!-- <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -173,5 +177,5 @@
                 </div>
             </div>
         </div>
-    </body>
+    </body> -->
 </html>
