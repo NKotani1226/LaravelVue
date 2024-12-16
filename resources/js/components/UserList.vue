bb477c0d8c7cdc7f0e9b07cf9e122c8b11ba2cd6
KOTANI\uvmpd <kofma91@gmail.com>
2024-12-16 10:26:12 +0900

    ユーザーリストページのルーティング、ファイル追加で疎通まで

---
+++ resources/js/components/UserList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="userList">
+    <h1>userlist!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+};
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
