38a365ccf87291c8bece536afa6c82b040209c24
KOTANI\uvmpd <kofma91@gmail.com>
2024-12-18 13:36:03 +0900

    axiosを用いてフロントエンドでAPIを使用する

--- resources/js/components/UserList.vue
+++ resources/js/components/UserList.vue
@@ -1,20 +1,47 @@
 <template>
   <div id="userList">
-    <div class="container mt-5">
-        <h1 class="mb-4">UserList</h1>
+    <div class="container mt-5 mb-5">
+        <h2>UserList</h2>
 
         <ul class="list-group">
-            <li class="list-group-item">Item 1</li>
-            <li class="list-group-item">Item 2</li>
-            <li class="list-group-item">Item 3</li>
+          <li class="list-group-item" v-for="user in users" :key="user.id">{{ user.name }}</li>
         </ul>
-
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+
 export default {
   name: 'UserList',
+  setup() {
+    const users = ref([]);
+
+    // APIを呼び出してデータを取得する
+    const fetchUsers = async () => {
+      try {
+        //WebAPI
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        users.value = response.data;
+
+        //API
+        const res = await axios.get("/api/apidata");
+        console.log(res);
+        
+      } catch (error) {
+        console.error('データ取得エラー:', error);
+      }
+    };
+
+    // コンポーネントがマウントされた時にデータを取得
+    onMounted(fetchUsers);
+
+    return {
+      users,
+    };
+  },
 };
 </script>
